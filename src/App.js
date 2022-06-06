ba72ef41c482741adb77849ab6e672590d84d71c
Bill Huang <a22687677@gmail.com>
2022-06-06 11:20:44 -0700

    Add searchbox to generate a repo pofolio for any valid github user

--- src/App.js
+++ src/App.js
@@ -9,18 +9,27 @@ function App() {
     loading: false,
     repos: null,
   });
-
+  
+  const [userid, setUserid] = useState('BillECU');
+  
   useEffect(() => {
     setAppState({ loading: true });
-    const apiUrl = `https://api.github.com/users/BillECU/repos`;
+    const apiUrl = (userid ==="")? `https://api.github.com/users/BillECU/repos`:`https://api.github.com/users/${userid}/repos`;
+    
     fetch(apiUrl)
       .then((res) => res.json())
       .then((repos) => {
         setAppState({ loading: false, repos: repos });
       });
-  }, [setAppState]);
+  }, [userid]);
+
   return (
     <div className='App'>
+      <form>
+        <input placeholder="GitHub id" onChange={e => setUserid(e.target.value)} />
+        {/* <input type="submit" /> */}
+      </form>
+      
       <div className='container'>
         <h1>My Repositories</h1>
       </div>
@@ -29,7 +38,7 @@ function App() {
       </div>
       <footer>
         <div className='footer'>
-          Built by <a href="https://github.com/BillECU">Bill Huang</a>
+          Built by <a href="https://github.com/${userid}">{userid}</a>
         </div>
       </footer>
     </div>
